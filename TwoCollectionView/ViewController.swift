e533269834a10f9c77826c145d454b37069e6dc7
Sani <sani@mudah.my>
2022-07-15 02:06:07 +0800

    Initial Commit

---
+++ TwoCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoCollectionView
+//
+//  Created by Abd Sani Abd Jalal on 15/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
